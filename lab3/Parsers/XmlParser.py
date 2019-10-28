d1758c2d4e78ddba980620597ef5687cc6e4effa
Roman <r.devyatilov@yandex.ru>
2019-10-28 05:27:32 +0300

    refactor and comments

--- lab3/Parsers/XmlParser.py
+++ lab3/Parsers/XmlParser.py
@@ -10,22 +10,21 @@ class XmlParser:
         self.attributes_regexp = re.compile(r'[0-9a-zA-Z]+=\"[^\"]*\"', OPTIONS)
         self.default_token_regexp = re.compile(r"<([0-9a-zA-Z]+)( *[0-9a-zA-Z]+=\"[^\"]*\")*>(?P<inner>.*)</\1>", OPTIONS)
 
+    # генератор, который парсит параметры тэга типа <key>="<value>"
     def parse_args(self, args):
         for group in args:
             key, value = group.split("=")
+            #
             value = value[1:-1]
-            # print(value)
             yield key, value
 
+    # получает имя и содержимое самозакрываемого тэга <tag/> так же возвращает строку без этого тэга
     def get_self_closed_token(self, s):
         s = s.strip()
         g = self.self_closed_token_regexp.match(s)
         if g is None:
             return None
-        # print(s, g.groups(), g.string[:g.end()])
         name = g.groups()[0]
-        groups = g.groups()[1]
-        # print(groups)
         args = self.attributes_regexp.findall(g.string[:g.end()])
         values = {}
         if args is not None:
@@ -33,17 +32,13 @@ class XmlParser:
                 values[key] = value
         return name, values, s[g.end():].strip()
 
+    # получает имя и содержимое блока, который закрыт тэгами вида <s> </s> так же возвращает строку без блока
     def get_default_token(self, s):
         s.strip()
         g = self.default_token_regexp.match(s)
-        # print(s, g)
         if g is None:
             return None
-        # print(g)
-
-        dictt = g.groupdict()
-        # print("dict", dictt)
-        values = self.parse(dictt["inner"].strip())[0]
+        values = self.parse(g.groupdict()["inner"].strip())[0]
         args = s.split('>')[0].split()[1:]
         if len(args) > 0:
             if str(type(values)) == "<class 'str'>":
@@ -54,19 +49,20 @@ class XmlParser:
                 values[key] = value
         return g.groups()[0], values, s[g.end():].strip()
 
+    # метод, который обобщает выше описанные
+    # вызывает по очереди методы для разных тэгов
+    # если ни один не сработал, значит вернём как строку внутри тэга
     def parse(self, src):
         res = {}
         if src == '':
             return None
         while src != '':
-            # print(src)
             temp = self.get_self_closed_token(src)
             if temp is not None:
                 key, value, src = temp
                 res[key] = value
                 continue
             temp = self.get_default_token(src)
-            # print(temp)
             if temp is not None:
                 key, value, src = temp
                 res[key] = value
